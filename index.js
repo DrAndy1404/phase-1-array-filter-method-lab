1187019c1beacdc11b78b1762096e2d953a6226c
Andrew <andrew.dimou10@gmail.com>
2024-03-14 13:50:59 +1100

    Initialize Git

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+const drivers = [
+  { name: "Helen", city: "Limassol" },
+  { name: "John", city: "Thessaloniki" },
+  { name: "Nick", city: "Athens" },
+  { name: "Michael", city: "Krete" },
+  { name: "Maria", city: "Piraeus" },
+];
+
+function findMatching(driversArray, query) {
+  return driversArray.filter(
+    (driver) => driver.toLowerCase() === query.toLowerCase()
+  );
+}
+
+function fuzzyMatch(driversArray, query) {
+  return driversArray.filter((driver) =>
+    driver.toLowerCase().startsWith(query.toLowerCase())
+  );
+}
+
+function matchName(driversArray, query) {
+  return driversArray.filter(
+    (driver) => driver.name.toLowerCase() === query.toLowerCase()
+  );
+}
